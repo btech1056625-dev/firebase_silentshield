49efd838d63da75ac06ecd8ca86d9e3f9bc9a1f5
Bhavya Varshney <btech10566.25@bitmesra.ac.in>
2026-03-14 23:28:19 +0530

    Update

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.DB_PASSWORD || 'Bh@vya12345',
     database: process.env.DB_NAME || 'riskengine_db',
     waitForConnections: true,
     connectionLimit: 10,
